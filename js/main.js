9e2c1d0645db8d44a4882a9a52186687e58279e0
Dziza Zhenya <86112194+ZenyaDAR@users.noreply.github.com>
2022-04-08 09:20:16 +0300

    Delete main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('.burger')
-const menu = document.querySelector('.header__menu')
-const body = document.querySelector('body')
-
-btn.addEventListener('click', () => {
-    btn.classList.toggle('active')
-    menu.classList.toggle('active')
-    body.classList.toggle('lock')
-})
